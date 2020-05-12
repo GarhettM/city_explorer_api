2a7a9bcc356bfa7bf74f9ac4649c0857ad8fc20a
GarhettM <32fossil@gmail.com>
2020-05-11 22:32:36 -0700

    add weather constructor

--- server.js
+++ server.js
@@ -17,10 +17,17 @@ function Location(param, city) {
   this.longitude = param.lon
 }
 
+function Weather(wParam, city)  {
+  this.search_query = city
+  this.formatted_query = wParam.display_name
+  this.description = wParam.data[0].weather.description
+  this.date = wParam.data.valid_date
+}
+
 app.get('/location', (req, res)  =>  {
   const location = require('./data/location.json');
   const city = req.query.city;
-  let newLocation = new Location(location[0], city)
+  let newLocation = new Location(location[0], city);
   
   // const getCoordLat = location[0].lat
   // const getCoordLon = location[0].lon
@@ -29,12 +36,20 @@ app.get('/location', (req, res)  =>  {
   // coordinates.push(getCoordLat);
   // coordinates.push(getCoordLon);
   // console.log(getCoordLat)
-
+  console.log(newLocation)
   res.send(newLocation);
 });
 
 app.get('/weather', (req, res) =>  {
   const weather = require('.data/weather.json')
+  const city = req.query.city;
+  const weatherArr  = [];
+  for (let i = 0; i < weather.length; i++)  {
+    let newWeather = new Weather(weather[i], city);
+    console.log(newWeather)
+    weatherArr.push(newWeather);
+  }
+  res.send(weatherArr);
 });
 
 
