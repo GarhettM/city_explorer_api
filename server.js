048b165b0b28ed6d1083aa4bd7b99fd480233288
GarhettM <32fossil@gmail.com>
2020-05-11 17:50:31 -0700

    location weather lab6

--- server.js
+++ server.js
@@ -10,13 +10,27 @@ const app = express();
 
 app.use(cors());
 
+function Location(param) {
+  this.formatted_query = param.display_name
+  this.latitude = param.lat
+}
 
 app.get('/location', (req, res)  =>  {
   const location = require('./data/location.json');
-  console.log(location);
+  const getCoordLat = location[0].lat
+  const getCoordLon = location[0].lon
+  const coordinates = [];
+
+  coordinates.push(getCoordLat);
+  coordinates.push(getCoordLon);
+  console.log(getCoordLat)
+
+  res.send(coordinates);
 });
 
-
+app.get('/weather', (req, res) =>  {
+  const weather = require('.data/weather.json')
+});
 
 
 
