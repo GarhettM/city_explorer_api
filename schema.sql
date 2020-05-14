c8d50ef5fa29f220b389503a9cdfc80f3ac06f23
GarhettM <32fossil@gmail.com>
2020-05-13 17:46:23 -0700

    sql server

---
+++ schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE locations;
+CREATE TABLE IF NOT EXISTS
+locations(
+  id SERIAL PRIMARY KEY,
+  search_query VARCHAR(255),
+  formatted_query VARCHAR(255),
+  lat NUMERIC,
+  lon NUMERIC
+);
+
+
